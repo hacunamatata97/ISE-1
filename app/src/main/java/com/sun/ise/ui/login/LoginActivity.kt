048d7ed08f8bac2b2ac25fb05477c06a13c4438c
hacunamatata97 <qwerty.lynkaa@gmail.com>
2019-06-19 17:10:28 +0700

    Refs #13288 Create Main Activity UI

--- app/src/main/java/com/sun/ise/ui/login/LoginActivity.kt
+++ app/src/main/java/com/sun/ise/ui/login/LoginActivity.kt
@@ -1,7 +1,7 @@
 package com.sun.ise.ui.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.sun.ise.R
 
 class LoginActivity : AppCompatActivity() {
