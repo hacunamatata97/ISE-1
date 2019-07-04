a703b0fae796bb29b4759b934b3c73c0121f769b
hacunamatata97 <qwerty.lynkaa@gmail.com>
2019-07-04 13:12:49 +0700

    Modify detail layout

--- app/src/main/java/com/sun/ise/ui/common/ViewPagerAdapter.kt
+++ app/src/main/java/com/sun/ise/ui/common/ViewPagerAdapter.kt
@@ -6,12 +6,19 @@ import androidx.fragment.app.FragmentPagerAdapter
 
 class ViewPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
     private val fragments = ArrayList<Fragment>()
+    private val tabTitles = ArrayList<String>()
 
     override fun getItem(position: Int): Fragment = fragments[position]
 
     override fun getCount(): Int = fragments.size
 
+    override fun getPageTitle(position: Int): CharSequence? = tabTitles[position]
+
     fun addFragment(vararg fragment: Fragment) {
         fragment.forEach { fragments.add(it) }
+    }
+
+    fun addTabTitles(vararg title: String) {
+        title.forEach { tabTitles.add(it) }
     }
 }
