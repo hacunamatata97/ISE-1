1b737e1796c39b575a0967554914415ff2516014
hacunamatata97 <qwerty.lynkaa@gmail.com>
2019-07-04 14:19:18 +0700

    Get detail data

--- app/src/main/java/com/sun/ise/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/sun/ise/ui/detail/DetailActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.PorterDuff
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
